20ef8ce21d72bba87843519e447286c5837b69da
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-09-03 11:03:14 +0200

    Edit action URLs for pull request to smart-grid-use-cases repo

--- get_latest_xml2md.sh
+++ get_latest_xml2md.sh
@@ -1,11 +1,10 @@
-latest_url=$(wget https://github.com/richardmarston/xml2md/releases/latest --max-redirect=0 2>&1 | grep Location)
-echo "latest url: $latest_url"
-prefix="Location: https://github.com/richardmarston/xml2md/releases/tag/"
+latest_url=$(wget https://github.com/smart-grid-use-cases/xml2md/releases/latest --max-redirect=0 2>&1 | grep Location)
+echo "Latest URL: $latest_url"
+prefix="Location: https://github.com/smart-grid-use-cases/xml2md/releases/tag/"
 suffix=" [following]"
 tail=${latest_url#"$prefix"}
-echo "tail: $tail"
 release_version=${tail%"$suffix"}
-echo "release version: $release_version"
-wget https://github.com/richardmarston/xml2md/releases/download/${release_version}/Bridge.zip
+echo "Release version: $release_version"
+wget https://github.com/smart-grid-use-cases/xml2md/releases/download/${release_version}/Bridge.zip
 unzip Bridge.zip -d content/en/docs/
 rm Bridge.zip
