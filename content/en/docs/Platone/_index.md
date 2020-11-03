468a5fc774fc008a5eb21619c3129e6a768d48fb
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-11-03 12:46:12 +0100

    Rename Platone to Use Cases

--- content/en/docs/Platone/_index.md
+++
@@ -1,5 +0,0 @@
----
-title: "PlatOne"
-linkTitle: "PlatOne"
-weight: 5
----
--- get_latest_xml2md.sh
+++ get_latest_xml2md.sh
@@ -1,3 +1,6 @@
+set -o nounset
+set -o errexit
+
 latest_url=$(wget https://github.com/smart-grid-use-cases/xml2md/releases/latest --max-redirect=0 2>&1 | grep Location)
 echo "Latest URL: $latest_url"
 prefix="Location: https://github.com/smart-grid-use-cases/xml2md/releases/tag/"
@@ -7,4 +10,9 @@ release_version=${tail%"$suffix"}
 echo "Release version: $release_version"
 wget https://github.com/smart-grid-use-cases/xml2md/releases/download/${release_version}/Bridge.zip
 unzip Bridge.zip -d content/en/docs/
+for dir in $(find content/en/docs/Bridge/ -mindepth 1 -maxdepth 1 -type d);
+do
+  cp -a $dir content/en/docs/UseCases
+done
 rm Bridge.zip
+rm -f content/en/docs/Bridge/_index.md
